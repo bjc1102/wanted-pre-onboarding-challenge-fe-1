d4f3dac44d75da75697e0fc48cb6dbd404a86707
bjc1102 <bjc30849@gmail.com>
2022-12-28 08:44:11 +0900

    feat: router 추가, router 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Auth from "./pages/Auth";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
       <h1 className="text-3xl font-bold underline">Hello Tailwind!</h1>
     </div>
   );
