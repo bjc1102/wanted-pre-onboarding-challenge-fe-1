14627c62c1bef0444d434bc3dac7a982be2aa9b9
bjc1102 <bjc30849@gmail.com>
2022-12-28 16:39:58 +0900

    chore: router 분리

---
+++ src/lib/Router.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Auth from "../pages/Auth";
+import Home from "../pages/Home";
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="" element={<Home />} />
+      <Route path="/auth/*" element={<Auth />} />
+    </Routes>
+  );
+};
+
+export default Router;
