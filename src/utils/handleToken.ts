5d79a26b3eb9f1d9f552e13a7afb07aeb5d2bb8a
bjc1102 <bjc30849@gmail.com>
2023-01-04 09:31:33 +0900

    feat: token 확인시 에러 메시지 추가

--- src/utils/handleToken.ts
+++ src/utils/handleToken.ts
@@ -1,10 +1,10 @@
 import history from "../lib/history";
 
-const handleToken = () => {
+const handleToken = (msg = "로그인이 필요한 서비스입니다") => {
   if (history.location.pathname !== "/auth") {
     localStorage.removeItem("token");
-    window.alert("로그인이 필요한 서비스입니다.");
     history.replace("/auth");
+    window.alert(msg);
   }
 };
 
