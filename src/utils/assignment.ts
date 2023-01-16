8b155c75515cc76f84be4d0ad3fe2cdf322dc887
bjc1102 <bjc30849@gmail.com>
2023-01-16 20:17:40 +0900

    fix: redux docs를 보고 리팩토링

--- src/utils/assignment.ts
+++ src/utils/assignment.ts
@@ -11,38 +11,54 @@ function todoList(state: any = [], action: any) {
   }
 }
 
-const createStore = (reducer: any, state: any): any => {
-  const storeState = reducer();
+const createStore = function <S, A>(
+  reducer: (state: S, action: A) => S,
+  preloadedState: S
+) {
+  const currentReducer = reducer;
+  let currentState = preloadedState as S;
+  let currentListeners: VoidFunction[] | null = [];
+  let nextListeners = currentListeners;
 
-  const subscribe = function (callback: VoidFunction = function () {}) {
-    return callback;
-  }; // store에 변화가 있을 때 실행되는 listener 함수
+  const getState = function (): S {
+    return currentState;
+  };
+  //상태를 업데이트 해줘야하고 상태를 업데이트 했다면 subscribe에 있는 함수들을 실행시켜준다
+  const dispatch = function (action: A): A {
+    currentState = currentReducer(currentState, action);
+    const listeners = (currentListeners = nextListeners);
+    if (listeners) listeners.forEach((listener) => listener());
 
-  const getState = function () {
-    return storeState;
-  }; // state를 반환하는 함수
-  const dispatch = function (action: { type: string; payload: any[] }) {
-    const result = reducer(storeState, action.type);
-
-    subscribe();
-
-    return result;
+    return action;
   };
 
-  return { subscribe, getState, dispatch };
+  const subscribe = function (listener: VoidFunction) {
+    // subscribe는 dispatch가 호출될 떄마다 실행되는 함수(정확히는 상태가 변경될때마다 실행됨)
+    // unsubscribe라는 함수를 반환하며 호출되는 함수를 지울 때 사용
+    nextListeners.push(listener);
+
+    return function unsubscribe() {
+      const index = nextListeners.indexOf(listener);
+      nextListeners.splice(index, 1);
+      currentListeners = null;
+    };
+  };
+
+  return { getState, dispatch, subscribe };
 };
 
 const store = createStore(todoList, []);
 
-store.getState();
-store.subscribe();
-store.dispatch({ type: "hello", payload: "" });
+// store.getState();
+// store.subscribe();
+// store.dispatch({ type: "hello", payload: "" });
 
 // const store = createStore((state, action) => {
 //   if (action.type === "ADD") {
 //     return { ...state, todos: [...state.todos, action.payload] };
 //   }
 //   return state;
+
 // });
 
 function test() {}
